9ce8633d66d14dc43513ec2611ceeaba93a1028a
Adrien BRAULT <monsti@gmail.com>
2011-06-03 10:17:07 +0200

    Hide fieldValues in article edit when subFamily is changed

--- web/assets/scripts.js
+++ web/assets/scripts.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
 
     console.log('Scripts successfully loaded.');
+
+    var article_fieldValues = $('#article_fieldValues').parent();
+    var article_subFamily = $('#article_subFamily');
+
+    var initialValue_article_subFamily = article_subFamily.val()
+    
+    article_subFamily.change(function(event) {
+        if ($(this).val() == initialValue_article_subFamily) {
+            article_fieldValues.show();
+        } else {
+            article_fieldValues.hide();
+        }
+    });
     
 });

02ed265a9c29367cf5b19c7611cf88e4a232359e
Adrien BRAULT <monsti@gmail.com>
2011-06-13 15:33:04 +0200

    Homepage complete

--- src/Supinfo/WebBundle/Controller/DefaultController.php
+++ src/Supinfo/WebBundle/Controller/DefaultController.php
@@ -9,6 +9,23 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('SupinfoWebBundle:Default:homepage.html.twig');
+        $nextLoans = null;
+        $currentLoans = null;
+
+        if ($this->get('security.context')->isGranted('ROLE_CLIENT')) {
+            $em = $em = $this->get('doctrine')->getEntityManager();
+            $entityRepository = $em->getRepository('SupinfoWebBundle:Loan');
+
+            $nextLoans = $entityRepository->get5NextLoans();
+            $currentLoans = $entityRepository->getCurrentLoans();
+        }
+
+        return $this->render(
+            'SupinfoWebBundle:Default:homepage.html.twig',
+            array(
+                'nextLoans' => $nextLoans,
+                'currentLoans' => $currentLoans,
+            )
+        );
     }
 }
--- src/Supinfo/WebBundle/Repository/LoanRepository.php
+++ src/Supinfo/WebBundle/Repository/LoanRepository.php
@@ -10,4 +10,53 @@ namespace Supinfo\WebBundle\Repository;
  */
 class LoanRepository extends EntityRepository
 {
+
+    public function get5NextLoansQB()
+    {
+        $qb = $this->selectQB();
+
+        $qb->orderBy(
+            $qb->getRootAlias().'.dateStart',
+            'ASC'
+        )->andWhere(
+            $qb->expr()->gt(
+                $qb->getRootAlias().'.dateStart',
+                'CURRENT_DATE()'
+            )
+        )->setMaxResults(
+            5
+        );
+
+        return $qb;
+    }
+
+    public function get5NextLoans()
+    {
+        return $this->get5NextLoansQB()->getQuery()->getResult();
+    }
+
+    public function getCurrentLoansQB()
+    {
+        $qb = $this->selectQB();
+
+        $qb->andWhere(
+            $qb->expr()->lt(
+                $qb->getRootAlias().'.dateStart',
+                'CURRENT_DATE()'
+            )
+        )->andWhere(
+            $qb->expr()->gt(
+                $qb->getRootAlias().'.dateEnd',
+                'CURRENT_DATE()'
+            )
+        );
+
+        return $qb;
+    }
+
+    public function getCurrentLoans()
+    {
+        return $this->getCurrentLoansQB()->getQuery()->getResult();
+    }
+
 }

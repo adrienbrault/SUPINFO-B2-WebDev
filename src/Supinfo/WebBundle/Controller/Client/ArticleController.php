cda2ffe7b0be85ba76660687f57f1efb00192a46
Adrien BRAULT <monsti@gmail.com>
2011-06-14 03:09:47 +0200

    Article list and view in client

--- src/Supinfo/WebBundle/Controller/Client/ArticleController.php
+++ src/Supinfo/WebBundle/Controller/Client/ArticleController.php
@@ -2,9 +2,34 @@
 
 namespace Supinfo\WebBundle\Controller\Client;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
-class ArticleController extends Controller
+class ArticleController extends EntityController
 {
-    
+
+    public function getEntityName()
+    {
+        return 'Article';
+    }
+
+    public function viewAction($id)
+    {
+        $this->fetchEntity(array('id' => $id));
+
+        return $this->render(
+            'SupinfoWebBundle:Client\Article:view.html.twig'
+        );
+    }
+
+    public function listAction($page)
+    {
+        $this->initPaginator(array(
+            'current_page' => $page
+        ));
+
+        $this->entities = $this->paginator->getCurrentPageResults();
+
+        return $this->render(
+            'SupinfoWebBundle:Client\Article:list.html.twig'
+        );
+    }
+
 }
